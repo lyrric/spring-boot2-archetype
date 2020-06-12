7b69799ee35cde527af19e79ecb070e4378eccc5
wangxiaodong <xx20510@163.com>
2020-06-12 17:18:11 +0800

    t

---
+++ src/main/resources/archetype-resources/src/main/java/Application.java
@@ -0,0 +1,25 @@
+package ${groupId};
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * Created on 2018/6/6.
+ *
+ * @author wangxiaodong
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "${groupId}.mapper")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/my.test/ITest.java
@@ -0,0 +1,22 @@
+package ${groupId}.my.test;
+
+
+import org.junit.Test;
+import ${groupId}.Application;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 测试类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ITest {
+
+    @Test
+    public void DaoTest(){
+
+    }
+
+}
