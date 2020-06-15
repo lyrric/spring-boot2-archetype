29a27951206d66fbb1e9e4ab6ba2bf7e7b693f0e
wangxiaodong <xx20510@163.com>
2020-06-15 10:18:40 +0800

    将Spring boot升级到2.3.0，更新相关依赖

--- src/main/resources/archetype-resources/src/main/java/plugin/ResponseWrapper.java
+++ src/main/resources/archetype-resources/src/main/java/plugin/ResponseWrapper.java
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * 封装返回数据
  * @author wangxiaodong
  */
-@ControllerAdvice(annotations = {Controller.class, RestController.class})
+@ControllerAdvice(annotations = {RestController.class})
 public class ResponseWrapper implements ResponseBodyAdvice<Object> {
 
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -33,23 +33,20 @@ public class ResponseWrapper implements ResponseBodyAdvice<Object> {
     public Object beforeBodyWrite(Object o, MethodParameter methodParameter,
                                   MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass,
                                   ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
-        //swagger接口不封装返回数据
-        String path = serverHttpRequest.getURI().getPath();
-        if("/swagger-resources".equals(path) || "/v2/api-docs".equals(path)){
-            return o;
-        }
         //避免二次封装
         if(o instanceof HttpResult){
             return o;
         }
         if(o instanceof String){
             try {
+                //这里如果直接返回String的话，会报错
+                serverHttpResponse.getHeaders().setContentType(MediaType.APPLICATION_JSON);
                 return objectMapper.writeValueAsString(HttpResult.success(o));
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
                 return null;
             }
         }
-        return $.HttpResult.success(o);
+        return HttpResult.success(o);
     }
 }
