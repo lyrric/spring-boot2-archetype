fa1e43ee47f27582d3137f536af4ce7ee3a9292a
xiaodong.wang <xiaodong.wang@aylaasia.com>
2021-12-09 15:59:55 +0800

    返回string

--- src/main/resources/archetype-resources/src/main/java/core/WebConfiguration.java
+++ src/main/resources/archetype-resources/src/main/java/core/WebConfiguration.java
@@ -1,19 +1,30 @@
 package ${groupId}.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * 全局默认配置类
  */
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer{
 
-    @Bean
-    public StringToResultHttpMessageConverter myConverter(ObjectMapper mapper) {
-        return new StringToResultHttpMessageConverter().setMapper(mapper);
+
+    /**
+     *  解决controller返回string时报错
+     *  由于StringConverter在converters中的位置靠前，导致在controller中直接返回string时，
+     *  spring会认为你要返回的是text/plan类型，无法转换为RestBaseResponse类型从而导致报错。
+     *  这里的逻辑就是将MappingJackson2HttpMessageConverter的位置靠前，优先将string返回成json类型
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.stream().filter(t -> t.getClass() == MappingJackson2HttpMessageConverter.class)
+                .findAny()
+                .ifPresent(t->converters.add(0, t));
     }
 }
